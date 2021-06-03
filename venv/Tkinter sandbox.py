055906a0bd9420cda6ce7e993b0a7983cd649a8b
Dominick Faust <dominickfaust@gmail.com>
2021-06-03 14:47:06 -0400

    Test Commit

---
+++ venv/Tkinter sandbox.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+test = Tk()
+test.mainloop()
+
+# Working Tkinter test.
+# To use in Pycharm: File > Settings > /Project Name/ > Python Interpreter > "+" > search "future" > click install package
