e6efd5357519155d017e81bd3d7360d6716ed803
Dominick Faust <38222114+DomFaust@users.noreply.github.com>
2021-09-02 13:35:02 -0400

    Delete Tkinter sandbox.py

--- venv/Tkinter sandbox.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-test = tk.Tk()
-test.mainloop()
-
-# Working Tkinter test.
-# To use in Pycharm: File > Settings > /Project Name/ > Python Interpreter > "+" > search "future" > click install package
