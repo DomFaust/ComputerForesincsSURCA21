6f35e2f977032857aa4c63cf55da341ebb9acd25
Dominick Faust <dominickfaust@gmail.com>
2021-10-11 18:32:53 -0400

    fixed up MD5 files and closes back to menu

--- venv/MD5GUI/MD5_GUI.py
+++ venv/MD5GUI/MD5_GUI.py
@@ -2,8 +2,43 @@ import hashlib
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 
+def MD5GUI(Frame):
+    GUI = tk.Tk()
+    GUI.title('Senior Project')
+    GUI.geometry("700x300")
+    GUI.config(background="white")
 
-def fileChooser(): #select files and add to file array
+    window = tk.Label(GUI, text="Calcuating MD5 of Files",
+                      width=100, height=4, fg="blue")
+
+    button1 = tk.Button(GUI,
+                        text="Browse for First File for MD5 Hash",
+                        command=lambda: [fileChooser(window,L1), switch(button1,button2)])
+    button2 = tk.Button(GUI,
+                        text="Recalculate MD5 Hash",
+                        command=lambda: [recalc(window,L1), switch2(button2,button3)])
+    button3 = tk.Button(GUI,
+                        text="MD5 Comparison",
+                        command=lambda: [MD5Comp(window)])
+    buttonExit = tk.Button(GUI,
+                           text="Exit",
+                           command=GUI.destroy)
+
+    window.grid(column=1, row=1)
+
+    button1.grid(column=1, row=2)
+    button2.grid(column=1, row=3)
+    button2["state"] = "disabled"
+    button3.grid(column=1, row=4)
+    button3["state"] = "disabled"
+
+    L1 = tk.Listbox(GUI, width=100, height=4)
+    L1.grid(column=1, row=5)
+
+    buttonExit.grid(column=1, row=6)
+
+
+def fileChooser(window, L1): #select files and add to file array
     global result
     global fileName
 
@@ -22,7 +57,7 @@ def fileChooser(): #select files and add to file array
     else:
         L1.insert(tk.END, fileName, result.hexdigest())
 
-def recalc():
+def recalc(window, L1):
     global result1
 
     result1 = hashlib.md5()
@@ -35,31 +70,22 @@ def recalc():
 
     L1.insert(tk.END, fileName, result1.hexdigest())
 
-def MD5Comp(): #empty until MD5 code
+def MD5Comp(window): #empty until MD5 code
     if result.hexdigest() == result1.hexdigest():
         window.configure(text = "The file has not been modified or tampered with")
     else:
         window.configure(text = "The file has been modified or tampered with")
 
-GUI = tk.Tk()
-GUI.title('Senior Project')
-GUI.geometry("700x300")
-GUI.config(background = "white")
 
-window = tk.Label(GUI, text = "Calcuating MD5 of Files",
-               width = 100, height = 4, fg = "blue")
 
-button1 = tk.Button(GUI,
-                    text = "Browse for First File for MD5 Hash",
-                    command = lambda:[fileChooser(),switch()])
-def switch():
+def switch(button1,button2):
     if button1["state"] == "normal":
         button1["state"] = "disabled"
         button2["state"] = "normal"
     else:
         button1["state"] = "normal"
 
-def switch2():
+def switch2(button2,button3):
     if button2["state"] == "normal":
         button2["state"] = "disabled"
         button3["state"] = "normal"
@@ -68,30 +94,9 @@ def switch2():
 
 
 
-button2 = tk.Button(GUI,
-                        text = "Recalculate MD5 Hash",
-                        command = lambda:[recalc(),switch2()])
-button3 = tk.Button(GUI,
-                    text = "MD5 Comparison",
-                    command = MD5Comp)
-buttonExit = tk.Button(GUI,
-                     text = "Exit",
-                     command = exit)
 
-window.grid(column=1, row=1)
+if __name__ == "__main__":
 
-button1.grid(column=1, row=2)
-button2.grid(column=1, row=3)
-button2["state"] = "disabled"
-button3.grid(column=1, row=4)
-button3["state"] = "disabled"
-
-L1 = tk.Listbox(GUI,width = 100, height= 4)
-L1.grid(column = 1, row = 5)
-
-buttonExit.grid(column=1, row=6)
-
-
-
+    MD5GUI(GUI)
 # Let the window wait for any events
-GUI.mainloop()
+    GUI.mainloop()
