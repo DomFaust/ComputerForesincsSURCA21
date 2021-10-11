07a8fa36f133eb4e272b97959807f222c6f0fab8
lamar.brown <lamar.brown@hcl.com>
2021-10-11 11:43:21 -0400

    Implementing the website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrackPasswordDetailsComponent } from './crack-password-details/crack-password-details.component';
+import { EncryptionDetailsComponent } from './encryption-details/encryption-details.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'password-cracking', component: CrackPasswordDetailsComponent},
+  { path: 'encryption', component: EncryptionDetailsComponent},
+  { path: 'about', component: InfoComponent },
+  { path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CrackPasswordComponent } from './home/crack-password/crack-password.component';
+import { EncryptionComponent } from './home/encryption/encryption.component';
+import { MatCardModule } from '@angular/material/card'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CrackPasswordDetailsComponent } from './crack-password-details/crack-password-details.component';
+import { EncryptionDetailsComponent } from './encryption-details/encryption-details.component';
+import { InfoComponent } from './info/info.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    CrackPasswordComponent,
+    EncryptionComponent,
+    CrackPasswordDetailsComponent,
+    EncryptionDetailsComponent,
+    InfoComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
