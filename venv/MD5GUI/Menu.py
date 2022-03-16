c484815bf0881fb715535973f92d1c3b7c8a28d6
Dominick Faust <dominickfaust@gmail.com>
2022-03-16 14:18:18 -0400

    Added brute force functionality

--- venv/MD5GUI/Menu.py
+++ venv/MD5GUI/Menu.py
@@ -13,51 +13,50 @@ from MD5GUI import Login
 from MD5GUI import BruteForce
 from datetime import date
 
-cred = firebase_admin.credentials.Certificate("C:\\Users\domin\Downloads\python-application-results-firebase-adminsdk-hwe4o-dbe377275c.json")
+cred = firebase_admin.credentials.Certificate("C:\\Users\domin\Downloads\python-application-results-firebase-adminsdk-hwe4o-dbe377275c.json") #json file needed to access database
 defaultApp = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://python-application-results-default-rtdb.firebaseio.com/'
-})
+})     #connects database to Python
 ref = db.reference("/")
 
-def browser():
+def browser(): #open up new window for broswer
     win = tk.Toplevel(GUI).withdraw()
     Browser.url(win)
 
-def sha():
+def sha(): #open up new window for SHA
     win = tk.Toplevel(GUI).withdraw()
     Sha.Sha(win,cred,defaultApp,ref)
 
-def MD5():
+def MD5(): #open up new window for MD5
     win = tk.Toplevel(GUI).withdraw()
     MD5_GUI.MD5GUI(win)
 
-def pdf():
+def pdf(): #open up new window for PDF
     win = tk.Toplevel(GUI).withdraw()
     PDF.pdf(win)
 
-def jpg():
+def jpg(): #open up new window for JPG
     win = tk.Toplevel(GUI).withdraw()
     exifData.jpg(win)
 
-def png():
+def png(): #open up new window for PNG
     win = tk.Toplevel(GUI).withdraw()
     PictureData.png(win)
 
-def brute():
+def brute(): #open up new window for Brute Force
     win = tk.Toplevel(GUI).withdraw()
     BruteForce.BruteForce(win)
 
 
-def login():
-    win = tk.Toplevel(GUI).withdraw()
-    Login.login(win)
+#def login():
+ #   win = tk.Toplevel(GUI).withdraw()
+  #  Login.login(win)
 
 if __name__ == "__main__":
     GUI = tk.Tk()
     GUI.title('Menu')
-    GUI.geometry("265x200")
+    GUI.geometry("265x200")                           #creates top level GUI
     GUI.config(background = "white")
-   # tk.Button(GUI,text="Return to Main Menu",command=GUI.deiconify)
     window = tk.Label(GUI, text = "Please Select What you Would Like to do",width=37,height=4,fg = "blue")
     window.grid(columnspan=2)
     md5Button = tk.Button(GUI, text="MD5 Comparison",command=MD5)
