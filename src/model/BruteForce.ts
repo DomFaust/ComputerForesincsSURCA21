600a4c31809018ac35b63677ecd0dbddf6813c37
lamar.brown <lamar.brown@hcl.com>
2022-04-27 16:26:12 -0400

    git push

---
+++ src/model/BruteForce.ts
@@ -0,0 +1,8 @@
+export class BruteForce{
+  Date_Time !: any;
+  ID !: any;
+  Decode_Time !: any;
+  File !: any;
+  Password !: any;
+  Wordlist !: any;
+}
---
+++ src/model/PDF.ts
@@ -0,0 +1,10 @@
+export class PDF{
+  Date_Time !: any;
+  ID !: any;
+  Author !: any;
+  CreationDate !: any;
+  Creator !: any;
+  ModDate !: any;
+  Producer !: any;
+  Title !: any;
+}
